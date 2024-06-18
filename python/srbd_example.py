677e2f9c38597fa714cb18a3a04618ec0c98bfc3
EnricoMingo <enricomingo@gmail.com>
2024-06-18 16:28:15 +0200

    separated visualizetion related methods from main

--- python/srbd_example.py
+++ python/srbd_example.py
@@ -9,18 +9,15 @@ from horizon.utils import utils, kin_dyn, resampler_trajectory, mat_storer
 from horizon.transcriptions.transcriptor import Transcriptor
 from horizon.solvers import solver
 from horizon.ros.replay_trajectory import *
-import matplotlib.pyplot as plt
-import os, time
+import time
 from horizon.ros import utils as horizon_ros_utils
 from ttictoc import tic,toc
 import tf
-from geometry_msgs.msg import WrenchStamped, Point, PoseStamped, TwistStamped
+from geometry_msgs.msg import WrenchStamped, PoseStamped, TwistStamped
 from sensor_msgs.msg import Joy
-from numpy import linalg as LA
-from visualization_msgs.msg import Marker, MarkerArray
-from abc import ABCMeta, abstractmethod
 from std_msgs.msg import Float32
 from scipy.spatial.transform import Rotation as R
+import viz
 
 SOLVER = lambda: 'ipopt'
 
@@ -174,87 +171,11 @@ def joy_cb(msg):
     global joy_msg
     joy_msg = msg
 
-def publishPointTrj(points, t, name, frame, color = [0.7, 0.7, 0.7]):
-    marker = Marker()
-    marker.header.frame_id = frame
-    marker.header.stamp = t
-    marker.ns = "SRBD"
-    marker.id = 1000
-    marker.type = Marker.LINE_STRIP
-    marker.action = Marker.ADD
-
-    for k in range(0, points.shape[1]):
-        p = Point()
-        p.x = points[0, k]
-        p.y = points[1, k]
-        p.z = points[2, k]
-        marker.points.append(p)
-
-    marker.color.a = 1.
-    marker.scale.x = 0.005
-    marker.color.r = color[0]
-    marker.color.g = color[1]
-    marker.color.b = color[2]
-
-    pub = rospy.Publisher(name + "_trj", Marker, queue_size=10).publish(marker)
-
-
-
-def publishContactForce(t, f, frame):
-    f_msg = WrenchStamped()
-    f_msg.header.stamp = t
-    f_msg.header.frame_id = frame
-    f_msg.wrench.force.x = f[0]
-    f_msg.wrench.force.y = f[1]
-    f_msg.wrench.force.z = f[2]
-    f_msg.wrench.torque.x = f_msg.wrench.torque.y = f_msg.wrench.torque.z = 0.
-    pub = rospy.Publisher('f' + frame, WrenchStamped, queue_size=10).publish(f_msg)
-
 def SRBDTfBroadcaster(r, o, c_dict, t):
     br = tf.TransformBroadcaster()
     br.sendTransform(r,o,t,"SRB","world")
     for key, val in c_dict.items():
         br.sendTransform(val, [0., 0., 0., 1.], t, key, "world")
-
-def SRBDViewer(I, base_frame, t, number_of_contacts):
-    marker = Marker()
-    marker.header.frame_id = base_frame
-    marker.header.stamp = t
-    marker.ns = "SRBD"
-    marker.id = 0
-    marker.type = Marker.SPHERE
-    marker.action = Marker.ADD
-    marker.pose.position.x = marker.pose.position.y = marker.pose.position.z = 0.
-    marker.pose.orientation.x = marker.pose.orientation.y = marker.pose.orientation.z = 0.
-    marker.pose.orientation.w = 1.
-    a = I[0,0] + I[1,1] + I[2,2]
-    marker.scale.x = 0.5*(I[2,2] + I[1,1])/a
-    marker.scale.y = 0.5*(I[2,2] + I[0,0])/a
-    marker.scale.z = 0.5*(I[0,0] + I[1,1])/a
-    marker.color.a = 0.8
-    marker.color.r = marker.color.g = marker.color.b = 0.7
-
-    pub = rospy.Publisher('box', Marker, queue_size=10).publish(marker)
-
-    marker_array = MarkerArray()
-    for i in range(0, number_of_contacts):
-        m = Marker()
-        m.header.frame_id = "c" + str(i)
-        m.header.stamp = t
-        m.ns = "SRBD"
-        m.id = i + 1
-        m.type = Marker.SPHERE
-        m.action = Marker.ADD
-        m.pose.position.x = marker.pose.position.y = marker.pose.position.z = 0.
-        m.pose.orientation.x = marker.pose.orientation.y = marker.pose.orientation.z = 0.
-        m.pose.orientation.w = 1.
-        m.scale.x = m.scale.y = m.scale.z = 0.04
-        m.color.a = 0.8
-        m.color.r = m.color.g = 0.0
-        m.color.b = 1.0
-        marker_array.markers.append(m)
-
-    pub2 = rospy.Publisher('contacts', MarkerArray, queue_size=10).publish(marker_array)
 
 def setWorld(frame, kindyn, q, base_link="base_link"):
     FRAME = kindyn.fk(frame)
@@ -869,10 +790,10 @@ while not rospy.is_shutdown():
     t = rospy.Time().now()
     SRBDTfBroadcaster(solution['r'][:, 0], solution['o'][:, 0], c0_hist, t)
     for i in range(0, nc):
-        publishContactForce(t, solution['f' + str(i)][:, 0], 'c' + str(i))
-        publishPointTrj(solution["c" + str(i)], t, 'c' + str(i), "world", color=[0., 0., 1.])
-    SRBDViewer(I, "SRB", t, nc)
-    publishPointTrj(solution["r"], t, "SRB", "world")
+        viz.publishContactForce(t, solution['f' + str(i)][:, 0], 'c' + str(i))
+        viz.publishPointTrj(solution["c" + str(i)], t, 'c' + str(i), "world", color=[0., 0., 1.])
+    viz.SRBDViewer(I, "SRB", t, nc) #TODO: should we use w_R_b * I * w_R_b.T?
+    viz.publishPointTrj(solution["r"], t, "SRB", "world")
 
     cc = dict()
     ff = dict()
